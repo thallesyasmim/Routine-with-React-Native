f8e7376c776764c5b529721f4858f18d3f0a7a4a
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-03-23 09:20:37 -0300

    home_styles

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 20
+    },
+
+    detailsContainer: {
+        padding: 15,
+        alignItems: 'center'
+    },
+    
+    title: {
+        color: '#000',
+        fontSize: 40,
+        fontFamily: 'Montserrat_700Bold',
+        textTransform: 'uppercase',
+        marginBottom: 20,
+    }
+})
+
+export default styles
